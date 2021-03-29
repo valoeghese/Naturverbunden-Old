f172d80853523036fc571e83c40d6a5b7b53edf5
valoeghese <valoeghesemods@gmail.com>
2021-03-29 22:17:56 +1300

    progress

---
+++ src/main/java/valoeghese/naturverbunden/block/NVBBlocks.java
@@ -0,0 +1,37 @@
+/*
+ * Naturverbunden
+ * Copyright (C) 2020 Valoeghese
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package valoeghese.naturverbunden.block;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class NVBBlocks {
+	private static Block register(String id, AbstractBlock.Settings settings) {
+		return Registry.register(Registry.BLOCK, new Identifier("nvb", id), new Block(settings));
+	}
+
+	public static final Block ITEM_BLOCK = register("item_block", AbstractBlock.Settings.of(Material.DECORATION)
+			.strength(0.5f)
+			.sounds(BlockSoundGroup.STONE));
+}
---
+++ src/main/java/valoeghese/naturverbunden/block/entity/ItemBlockEntity.java
@@ -0,0 +1,97 @@
+/*
+ * Lint
+ * Copyright (C) 2020 hYdos, Valoeghese, ramidzkh
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package valoeghese.naturverbunden.block.entity;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+
+public class ItemBlockEntity extends BlockEntity {
+	public ItemBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+		super(type, pos, state);
+		this.items = DefaultedList.ofSize(3, ItemStack.EMPTY);
+	}
+
+	private final DefaultedList<ItemStack> items;
+
+	public DefaultedList<ItemStack> getItems() {
+		return this.items;
+	}
+
+	public boolean addItem(ItemStack item, int integer) {
+		for(int i = 0; i < this.items.size(); ++i) {
+			ItemStack itemStack = this.items.get(i);
+
+			if (itemStack.isEmpty()) {
+				this.items.set(i, item.split(1));
+				this.updateListeners();
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private void updateListeners() {
+		this.markDirty();
+		this.getWorld().updateListeners(this.getPos(), this.getCachedState(), this.getCachedState(), 3);
+	}
+
+
+	public void clear() {
+		this.items.clear();
+	}
+
+	// NBT Hell
+
+	public void readNbt(NbtCompound tag) {
+		super.readNbt(tag);
+		this.items.clear();
+		Inventories.readNbt(tag, this.items);
+	}
+
+	public NbtCompound writeNbt(NbtCompound tag) {
+		this.saveInitialChunkData(tag);
+		return tag;
+	}
+
+	private NbtCompound saveInitialChunkData(NbtCompound tag) {
+		super.writeNbt(tag);
+		Inventories.writeNbt(tag, this.items, true);
+		return tag;
+	}
+
+	@Nullable
+	public BlockEntityUpdateS2CPacket toUpdatePacket() {
+		return new BlockEntityUpdateS2CPacket(this.pos, 13, this.toInitialChunkDataNbt());
+	}
+
+	public NbtCompound toInitialChunkDataNbt() {
+		return this.saveInitialChunkData(new NbtCompound());
+	}
+}
